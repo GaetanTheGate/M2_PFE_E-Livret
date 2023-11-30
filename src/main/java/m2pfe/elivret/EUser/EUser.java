de6944b72152ce93dac4e8ff13a37d8d9303c00b
GaetanTheGate <gaetan.tr.pu@gmail.com>
2023-11-30 17:10:10 +0100

    Added EUser to the project

--- src/main/java/m2pfe/elivret/EUser/EUser.java
+++ src/main/java/m2pfe/elivret/EUser/EUser.java
@@ -1,5 +1,53 @@
 package m2pfe.elivret.EUser;
 
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <p>
+ * Entity used to represent an user in the application.
+ * </p>
+ * <p>
+ * An EUser <b>primary key</b> is its <b>id</b>.
+ * </p>
+ * 
+ * @author Gaëtan PUPET
+ * @version 1.0
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EUser {
-    
+    /**
+     * Primary key of the EUser.
+     * Used to identify an EUser.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    /**
+     * The email of the EUser.
+     * It is used to log in the application.
+     */
+    @Basic
+    @NotBlank
+    private String email; // TODO: rendre unique
+
+    /**
+     * The password of the EUser.
+     * It is ised to log in the application.
+     */
+    @Basic
+    @NotBlank
+    private String password;
 }
--- src/main/java/m2pfe/elivret/EUser/EUserRepository.java
+++ src/main/java/m2pfe/elivret/EUser/EUserRepository.java
@@ -1,5 +1,42 @@
 package m2pfe.elivret.EUser;
 
-public class EUserRepository {
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * Repository (DAO) for the EUser entity.
+ * </p>
+ * <p>
+ * It is used to access and manipulate the EUsers in the database, set in the configuration. 
+ * </p>
+ * 
+ * @see EUser
+ * @see JpaRepository
+ * 
+ * @author Gaëtan PUPET
+ * @version 1.0
+ */
+@Repository
+@Transactional
+public interface EUserRepository extends JpaRepository<EUser, Integer> {
     
+    /**
+     * <p>
+     * Fetch and return the user with the email passed in parameter.
+     * </p>
+     * 
+     * @see EUser
+     * 
+     * @param email The email associated with the user you wish to find.
+     * @return <ul>
+     *  <li>The <i>EUser</i> found.</li>
+     *  <li><i>null</i> if nothing was found</li>
+     * </ul>
+     */
+    @Query("SELECT u FROM EUser u WHERE u.email = :email")
+    public EUser findByEmail(String email); 
 }
