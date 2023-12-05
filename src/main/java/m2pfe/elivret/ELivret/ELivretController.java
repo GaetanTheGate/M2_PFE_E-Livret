854af2761ef7d00afed2c67fef50fd9ac932d0bb
GaetanTheGate <gaetan.tr.pu@gmail.com>
2023-12-05 10:54:10 +0100

    Set up the controllers for Authorization

--- src/main/java/m2pfe/elivret/ELivret/ELivretController.java
+++ src/main/java/m2pfe/elivret/ELivret/ELivretController.java
@@ -3,32 +3,57 @@ package m2pfe.elivret.ELivret;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import m2pfe.elivret.Authentification.AuthentificationException;
+import m2pfe.elivret.Authentification.AuthentificationService;
+import m2pfe.elivret.Authentification.EntityAccessAuthorization;
+import m2pfe.elivret.EUser.EUser;
+
 import org.modelmapper.ModelMapper;
 
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/livrets")
 public class ELivretController {
+    @Autowired
+    private EntityAccessAuthorization authorization;
 
     @Autowired
-    ELivretRepository l_repo;
+    private AuthentificationService service;
+
+    @Autowired
+    private ELivretRepository l_repo;
 
     private ModelMapper mapper = new ModelMapper();
 
     /// GetMapping
 
-    @GetMapping("")
+    @Deprecated
+    @GetMapping("/getAll")
     public List<ELivret> getLivrets() {
-        return l_repo.findAll();
-        // return l_repo.findAll().stream().map(l -> mapper.map(l, ELivret.class)).toList();
+        return l_repo.findAll().stream().map(l -> mapper.map(l, ELivret.class)).toList();
+    }
+
+    @GetMapping("")
+    public List<ELivret> getMyLivrets(HttpServletRequest req) throws AuthentificationException, ELivretException {
+        EUser me = service.whoAmI(req);
+        
+        return null;
     }
 
     @GetMapping("/{id}")
-    public ELivret getLivret(@PathVariable int id) throws ELivretNotFoundException {
+    public ELivret getLivret(@PathVariable int id, HttpServletRequest req) throws AuthentificationException, ELivretException {
+        if(!authorization.isMeFromLivret(req, id))
+            throw new AuthentificationException(HttpStatus.FORBIDDEN, "Not allowed to access this entity.");
+
         Optional<ELivret> l = l_repo.findById(id);
-        l.orElseThrow(ELivretNotFoundException::new);
+        l.orElseThrow(() -> new ELivretException(HttpStatus.NO_CONTENT, "Livret not found."));
+
+
         return mapper.map(l.get(), ELivret.class);
     }
 
@@ -36,10 +61,12 @@ public class ELivretController {
     /// PostMapping
 
     @PostMapping("")
-    public ELivret postLivret(@RequestBody ELivret livret) throws ELivretNotFoundException {
+    public ELivret postLivret(@RequestBody ELivret livret, HttpServletRequest req) throws AuthentificationException, ELivretException {
         ELivret l = mapper.map(livret, ELivret.class);
 
-        Optional.ofNullable(l_repo.findById(l.getId()).isPresent() ? null : l).orElseThrow(ELivretNotFoundException::new);
+        Optional.ofNullable(l_repo.findById(l.getId()).isPresent() ? null : l)
+            .orElseThrow(() -> new ELivretException(HttpStatus.NO_CONTENT, "Livret already exist."));
+        
         return l_repo.save(l);
     }
 
@@ -47,11 +74,12 @@ public class ELivretController {
     /// PutMapping
 
     @PutMapping("")
-    public ELivret putLivret(@RequestBody ELivret livret) throws ELivretNotFoundException {
+    public ELivret putLivret(@RequestBody ELivret livret, HttpServletRequest req) throws AuthentificationException, ELivretException {
 
         ELivret l = mapper.map(livret, ELivret.class);
 
-        l_repo.findById(l.getId()).orElseThrow(ELivretNotFoundException::new);
+        l_repo.findById(l.getId())
+            .orElseThrow(() -> new ELivretException(HttpStatus.NO_CONTENT, "Livret not found."));
 
         return l_repo.save(l);
     }
@@ -61,13 +89,7 @@ public class ELivretController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteLivret(@PathVariable int id) {
+    public void deleteLivret(@PathVariable int id, HttpServletRequest req) throws AuthentificationException {
         l_repo.deleteById(id);
-    }
-
-    /// Exception
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public class ELivretNotFoundException extends RuntimeException{
-        private static final long serialVersionUID = 1L;
     }
 }
--- src/main/java/m2pfe/elivret/ESection/ESectionController.java
+++ src/main/java/m2pfe/elivret/ESection/ESectionController.java
@@ -5,30 +5,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import m2pfe.elivret.Authentification.AuthentificationException;
+import m2pfe.elivret.Authentification.AuthentificationService;
+import m2pfe.elivret.Authentification.EntityAccessAuthorization;
+
 import java.util.List;
 import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/sections")
 public class ESectionController {
+    @Autowired
+    private EntityAccessAuthorization authorization;
 
     @Autowired
-    ESectionRepository s_repo;
+    private AuthentificationService service;
+
+    @Autowired
+    private ESectionRepository s_repo;
 
     private ModelMapper mapper = new ModelMapper();
 
     /// GetMapping
 
-    @GetMapping("")
+    @Deprecated
+    @GetMapping("/getAll")
     public List<ESection> getSections() {
         return s_repo.findAll().stream().map(p -> mapper.map(p, ESection.class))
                 .toList();
     }
 
+    @GetMapping("/livret/{id}")
+    public List<ESection> getLivretSection(@PathVariable Integer id, HttpServletRequest req) throws AuthentificationException, ESectionException {
+
+        return null;
+    }
+
+    @GetMapping("/livret/{id}/modify")
+    public List<ESection> getMySections(@PathVariable Integer id, HttpServletRequest req) throws AuthentificationException, ESectionException {
+
+        return null;
+    }
+
     @GetMapping("/{id}")
-    public ESection getSection(@PathVariable int id) throws ESectionNotFoundException {
+    public ESection getSection(@PathVariable int id, HttpServletRequest req) throws AuthentificationException, ESectionException {
         Optional<ESection> s = s_repo.findById(id);
-        s.orElseThrow(ESectionNotFoundException::new);
+        s.orElseThrow(() -> new ESectionException(HttpStatus.NO_CONTENT, "Section not found."));
 
         return mapper.map(s.get(), ESection.class);
     }
@@ -37,10 +61,12 @@ public class ESectionController {
     /// PostMapping
 
     @PostMapping("")
-    public ESection postSection(@RequestBody ESection section) throws ESectionNotFoundException {
+    public ESection postSection(@RequestBody ESection section, HttpServletRequest req) throws AuthentificationException, ESectionException {
         ESection s = mapper.map(section, ESection.class);
 
-        Optional.ofNullable(s_repo.findById(s.getId()).isPresent() ? null : s).orElseThrow(ESectionNotFoundException::new);
+        Optional.ofNullable(s_repo.findById(s.getId()).isPresent() ? null : s)
+            .orElseThrow(() -> new ESectionException(HttpStatus.NO_CONTENT, "Section already exist."));
+
         return s_repo.save(s);
     }
 
@@ -48,11 +74,12 @@ public class ESectionController {
     /// PutMapping
 
     @PutMapping("")
-    public ESection putSection(@RequestBody ESection section) throws ESectionNotFoundException {
+    public ESection putSection(@RequestBody ESection section, HttpServletRequest req) throws AuthentificationException, ESectionException {
 
         ESection s = mapper.map(section, ESection.class);
 
-        s_repo.findById(s.getId()).orElseThrow(ESectionNotFoundException::new);
+        s_repo.findById(s.getId())
+            .orElseThrow(() -> new ESectionException(HttpStatus.NO_CONTENT, "Section not found."));
 
         return s_repo.save(s);
     }
@@ -62,17 +89,7 @@ public class ESectionController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteSection(@PathVariable int id) {
+    public void deleteSection(@PathVariable int id, HttpServletRequest req) throws AuthentificationException {
         s_repo.deleteById(id);
     }
-
-
-    /// Exception
-
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public class ESectionNotFoundException extends RuntimeException{
-        private static final long serialVersionUID = 1L;
-    }
-
-
 }
