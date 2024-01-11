acec975a9fbd6693925a90e73cef9c52f5cd4ea9
GaetanTheGate <gaetan.tr.pu@gmail.com>
2024-01-11 15:50:45 +0100

    Button to switch to edition mode

--- vue-front/src/components/livretDetails.vue
+++ vue-front/src/components/livretDetails.vue
@@ -1,6 +1,8 @@
 <template>
     <!-- <button v-on:click="fetchLivret()">SHOW SECTIONS</button> -->
     <div v-if="livret" class="container">
+        <button v-if="!editionMode" v-on:click="setEditionMode(true)">Editer</button>
+        <button v-if=" editionMode" v-on:click="setEditionMode(false)">Consulter</button>
         <ul class="myUL">
             <li>student : {{ livret.student }}</li>
             <li>master : {{ livret.master }}</li>
@@ -9,7 +11,7 @@
             <li>id : {{ livret.id }}</li>
         </ul>
         <div v-for="section in livret.sections" :key="section.id" class="myUL">
-            <sectionDetails :sectionId="section.id" />
+            <sectionDetails :sectionId="section.id" :editionMode=editionMode />
         </div>
     </div>
 </template>
@@ -25,24 +27,30 @@ export default {
         livretId: {
             type: Number,
             required: true,  
-        },
+        }
     },
 
     data() {
         return {
-            livret: null,
+            livret:         null,
+            editionMode:    null,
         }
     },
     mounted() {
+        this.setEditionMode(false);
         this.fetchLivret();
     },
 
     methods: {
-        fetchLivret: function () {
+        fetchLivret: function() {
             let id = this["livretId"]
             this.$axiosApi.get("livrets/" + id).then(s => {
                 this.livret = s.data
             });
+        },
+
+        setEditionMode: function(state) {
+            this.editionMode = state;
         }
     },
     watch: {
--- vue-front/src/components/sectionDetails.vue
+++ vue-front/src/components/sectionDetails.vue
@@ -24,7 +24,7 @@
             },
             editionMode: {
                 type: Boolean,
-                default: true,
+                default: false,
             }
         },
 
