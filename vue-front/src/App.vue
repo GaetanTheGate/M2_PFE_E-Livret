37e7370fc14ea7ad5ed5636b5f7a0532b954ca5a
GaetanTheGate <gaetan.tr.pu@gmail.com>
2024-02-17 18:26:01 +0100

    Front - Sligthly better navbar

--- vue-front/src/App.vue
+++ vue-front/src/App.vue
@@ -1,36 +1,124 @@
 <template>
-  <nav id="nav" class="navbar navbar-expand-lg bd-navbar sticky-top" style="background-color: #712cf9">
+  <nav v-if="true" class="navbar nav-bar-expand sticky-top" style="background-color: #712cf9">
+    <div class="container-fluid d-flex justify-content-between p-2">
+      <div class=" d-flex justify-content-start">
+        <router-link to="/" class="nav-item m-1">
+          <button type="button" class="btn btn-outline-dark p-3">
+            Home
+          </button>
+        </router-link>
+
+        <router-link :to="{ name: 'About' }" class="nav-item m-1">
+          <button type="button" class="nav-item btn btn-outline-dark p-3">
+            About
+          </button>
+        </router-link>
+
+        <router-link :to="{ name: 'ListLivrets' }" class="nav-item m-1">
+          <button type="button" class="nav-item btn btn-outline-dark p-3">
+            Mes livrets
+          </button>
+        </router-link>
+
+      </div>
+
+      <div class=" d-flex justify-content-start">
+
+        <div class="nav-item m-1">
+          <button type="button" class="nav-item btn btn-outline-warning p-3"
+              v-on:click="this.$loginService.login(`etudiant2@mail.com`, `etudiant2`, fetchCurrentUser)">
+            Etudiant
+          </button>
+        </div>
+
+        <div class="nav-item m-1">
+          <button type="button" class="nav-item btn btn-outline-warning p-3"
+              v-on:click="this.$loginService.login(`maitre@mail.com`, `maitre`, fetchCurrentUser)">
+            Maitre
+          </button>
+        </div>
+
+        <div class="nav-item m-1">
+          <button type="button" class="nav-item btn btn-outline-warning p-3"
+              v-on:click="this.$loginService.login(`tuteur@mail.com`, `tuteur`, fetchCurrentUser)">
+            Tuteur
+          </button>
+        </div>
+
+        <div class="nav-item m-1">
+          <button type="button" class="nav-item btn btn-outline-warning p-3"
+              v-on:click="this.$loginService.login(`responsable@mail.com`, `responsable`, fetchCurrentUser)">
+            Tuteur
+          </button>
+        </div>
+
+      </div>
+
+      <div class=" d-flex justify-content-start">
+        <router-link v-if="!this.currentUser" :to="{ name: 'Login' }" class="nav-item m-1">
+          <button type="button" class="btn btn-outline-dark p-3">Se connecter</button>
+        </router-link>
+
+        <router-link :to="{ name: 'Profile' }" class="nav-item m-1">
+          <button v-if="this.currentUser" type="button" class="nav-item btn btn-outline-dark p-3">
+            Mon profile
+          </button>
+        </router-link>
+
+        <div class="nav-item m-1">
+          <button v-if="this.currentUser" type="button" class="nav-item btn btn-outline-dark p-3"
+            v-on:click="this.$loginService.logout(this.$pageService.gotoLoginPage.bind(this.$pageService))">
+            Se déconnecter
+          </button>
+        </div>
+
+      </div>
+    </div>
+  </nav>
+
+  <nav v-if="false" id="nav" class="navbar navbar-expand-lg bd-navbar sticky-top" style="background-color: #712cf9">
     <div class="mx-auto p-2">
-      <ul class="navbar-nav me-auto">
+      <ul class="navbar-nav me-auto d-flex justify-content-between">
+        <div>
+
+          <li class="nav-item">
+            <a>
+              <router-link to="/" class="nav-link">Home</router-link>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a>
+              <router-link :to="{ name: 'About' }" class="nav-link">About</router-link>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a>
+              <router-link :to="{ name: 'ListLivrets' }" class="nav-link">Livrets</router-link>
+            </a>
+          </li>
+        </div>
+        <div>
+        </div>
+        <div>
+          <li v-if="this.currentUser" class="nav-item">
+            <a>
+              <router-link :to="{ name: 'Profile' }" class="nav-link">Mon Profil</router-link>
+            </a>
+          </li>
+          <li v-if="this.currentUser" class="nav-item">
+            <a>
+              <button type="button"
+                v-on:click="this.$loginService.logout(this.$pageService.gotoLoginPage.bind(this.$pageService))"
+                class="nav-link">Logout</button>
+            </a>
+          </li>
+        </div>
 
         <li class="nav-item">
           <a>
-            <router-link to="/" class="nav-link">Home</router-link>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a>
-            <router-link :to="{ name: 'About' }" class="nav-link">About</router-link>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a>
-            <router-link :to="{ name: 'ListLivrets' }" class="nav-link">Livrets</router-link>
-          </a>
-        </li>
-        <li v-if="this.currentUser" class="nav-item">
-          <a>
-            <router-link :to="{ name: 'Profile' }" class="nav-link">Mon Profil</router-link>
-          </a>
-        </li>
-        <li v-if="this.currentUser" class="nav-item">
-          <a>
-            <button type="button" v-on:click="this.$loginService.logout(this.$pageService.gotoLoginPage.bind(this.$pageService))" class="nav-link">Logout</button>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a>
-            <button type="button" v-on:click="this.$loginService.login(`etudiant2@mail.com`, `etudiant2`, fetchCurrentUser)"
+            <button type="button"
+              v-on:click="this.$loginService.login(`etudiant2@mail.com`, `etudiant2`, fetchCurrentUser)"
               class="nav-link">Student</button>
           </a>
         </li>
@@ -48,7 +136,8 @@
         </li>
         <li class="nav-item">
           <a>
-            <button type="button" v-on:click="this.$loginService.login(`responsable@mail.com`, `responsable`, fetchCurrentUser)"
+            <button type="button"
+              v-on:click="this.$loginService.login(`responsable@mail.com`, `responsable`, fetchCurrentUser)"
               class="nav-link">Responsable</button>
           </a>
         </li>
@@ -96,7 +185,7 @@ export default {
     },
   },
   watch: {
-    
+
   }
 }
 </script>
@@ -127,12 +216,4 @@ export default {
 /*  color: white;*/
 /*  background: crimson;*/
 /*}*/
-
-.nav-link {
-  color: white;
-}
-
-.navbar {
-  align-content: center;
-}
 </style>
