99bca27dcd1d4b33f2845cf3704a416cc4ee1579
GaetanTheGate <gaetan.tr.pu@gmail.com>
2024-03-22 16:19:27 +0100

    Back - Re-added another function that was missing (isAnswerMine)

--- spring-back/src/main/java/m2pfe/elivret/Authentification/EntityAccessAuthorization.java
+++ spring-back/src/main/java/m2pfe/elivret/Authentification/EntityAccessAuthorization.java
@@ -276,6 +276,304 @@ public class EntityAccessAuthorization {
 
         /**
          * <p>
+         * Tells if the authenticated user matches the owner of a livret.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see ELivret
+         * 
+         * @param req The request where to find the token of the authenticated user.
+         * @param id  The id of the Livret to check from.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         */
+        public boolean isLivretMine(HttpServletRequest req, Integer id)
+                        throws AuthentificationException, EUserException, ELivretException {
+                EUser ru = service.whoAmI(req);
+
+                ELivret l = lr.findById(id)
+                                .orElseThrow(() -> new ELivretException(HttpStatus.NO_CONTENT,
+                                                "Livret's id not found."));
+
+                EUser lu = ur.findById(l.getUserFromRole(ELivret.UserRole.RESPONSABLE).getId())
+                                .orElseThrow(() -> new EUserException(HttpStatus.NO_CONTENT,
+                                                "Livret's owner not found."));
+
+                return ru.getId() == lu.getId();
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of a livret.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see ELivret
+         * @see #isLivretMine(HttpServletRequest, Integer)
+         * 
+         * @param req    The request where to find the token of the authenticated user.
+         * @param livret The livret to check from.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         */
+        public boolean isLivretMine(HttpServletRequest req, ELivret livret)
+                        throws AuthentificationException, EUserException, ELivretException {
+                return isLivretMine(req, livret.getId());
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of a livret.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see ELivret
+         * @see #isLivretMine(HttpServletRequest, Integer)
+         * 
+         * @param req     The request where to find the token of the authenticated user.
+         * @param section The section to check from.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         * @throws ESectionException
+         */
+        public boolean isLivretMine(HttpServletRequest req, ESection section)
+                        throws AuthentificationException, EUserException, ELivretException, ESectionException {
+                return isLivretMine(req, sr.findById(section.getId())
+                                .orElseThrow(() -> new ESectionException(HttpStatus.NO_CONTENT,
+                                                "Section's id not found."))
+                                .getLivret());
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of a livret.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see ELivret
+         * @see #isLivretMine(HttpServletRequest, Integer)
+         * 
+         * @param req      The request where to find the token of the authenticated
+         *                 user.
+         * @param question The question to check from.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         * @throws ESectionException
+         * @throws EQuestionException
+         */
+        public boolean isLivretMine(HttpServletRequest req, EQuestion question)
+                        throws AuthentificationException, EUserException, ELivretException, ESectionException,
+                        EQuestionException {
+                return isLivretMine(req, qr.findById(question.getId())
+                                .orElseThrow(() -> new EQuestionException(HttpStatus.NO_CONTENT,
+                                                "Question's id not found."))
+                                .getSection());
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of a livret.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see ELivret
+         * @see #isLivretMine(HttpServletRequest, Integer)
+         * 
+         * @param req    The request where to find the token of the authenticated user.
+         * @param answer The answer to check from.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         * @throws ESectionException
+         * @throws EQuestionException
+         * @throws EAnswerException
+         */
+        public boolean isLivretMine(HttpServletRequest req, EAnswer answer) throws AuthentificationException,
+                        EUserException, ELivretException, ESectionException, EQuestionException, EAnswerException {
+                return isLivretMine(req, ar.findById(answer.getId())
+                                .orElseThrow(() -> new EAnswerException(HttpStatus.NO_CONTENT,
+                                                "Answer's id not found."))
+                                .getQuestion());
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of the section.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see Esection
+         * 
+         * @param req The request where to find the token of the authenticated user.
+         * @param id  The id of the section to check.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         * @throws ESectionException
+         */
+        public boolean isSectionMine(HttpServletRequest req, Integer id)
+                        throws AuthentificationException, EUserException, ELivretException, ESectionException {
+                EUser ru = service.whoAmI(req);
+
+                ESection s = sr.findById(id)
+                                .orElseThrow(() -> new ESectionException(HttpStatus.NO_CONTENT,
+                                                "Section's id not found."));
+
+                ELivret l = lr.findById(s.getLivret().getId())
+                                .orElseThrow(() -> new ELivretException(HttpStatus.NO_CONTENT,
+                                                "Livret's id not found."));
+
+                EUser su = ur.findById(l.getUserFromRole(s.getOwner()).getId())
+                                .orElseThrow(() -> new EUserException(HttpStatus.NO_CONTENT, "User's id not found."));
+
+                return ru.getId() == su.getId();
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of the section.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see Esection
+         * 
+         * @param req     The request where to find the token of the authenticated user.
+         * @param section The section to check.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         * @throws ESectionException
+         */
+        public boolean isSectionMine(HttpServletRequest req, ESection section)
+                        throws AuthentificationException, EUserException, ELivretException, ESectionException {
+                return isSectionMine(req, section.getId());
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of the question.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see EQuestion
+         * 
+         * @param req The request where to find the token of the authenticated user.
+         * @param id  The of the question to check.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         * @throws ESectionException
+         * @throws EQuestionException
+         */
+        public boolean isQuestionMine(HttpServletRequest req, Integer id)
+                        throws AuthentificationException, EUserException, ELivretException, ESectionException,
+                        EQuestionException {
+                EQuestion question = qr.findById(id)
+                                .orElseThrow(() -> new EQuestionException(HttpStatus.NO_CONTENT,
+                                                "Question's id not found."));
+
+                return isSectionMine(req, question.getSection());
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of the question.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see EQuestion
+         * 
+         * @param req      The request where to find the token of the authenticated
+         *                 user.
+         * @param question The question to check.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         * @throws ESectionException
+         * @throws EQuestionException
+         */
+        public boolean isQuestionMine(HttpServletRequest req, EQuestion question)
+                        throws AuthentificationException, EUserException, ELivretException, ESectionException,
+                        EQuestionException {
+                return isQuestionMine(req, question.getId());
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of the answer.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see EAnswer
+         * 
+         * @param req The request where to find the token of the authenticated user.
+         * @param id  The id of the answer to check.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         * @throws ESectionException
+         * @throws EQuestionException
+         * @throws EAnswerException
+         */
+        public boolean isAnswerMine(HttpServletRequest req, Integer id)
+                        throws AuthentificationException, EUserException,
+                        ELivretException, ESectionException, EQuestionException, EAnswerException {
+                EAnswer answer = ar.findById(id)
+                                .orElseThrow(() -> new EAnswerException(HttpStatus.NO_CONTENT,
+                                                "Answer's id not found."));
+
+                return isQuestionMine(req, answer.getQuestion());
+        }
+
+        /**
+         * <p>
+         * Tells if the authenticated user matches the owner of the answer.
+         * </p>
+         * 
+         * @see HttpServletRequest
+         * @see EAnswer
+         * 
+         * @param req    The request where to find the token of the authenticated user.
+         * @param answer The answer to check.
+         * @return <i>true</i> if it matches, <i>false</i> otherwise.
+         * 
+         * @throws AuthentificationException
+         * @throws EUserException
+         * @throws ELivretException
+         * @throws ESectionException
+         * @throws EQuestionException
+         * @throws EAnswerException
+         */
+        public boolean isAnswerMine(HttpServletRequest req, EAnswer answer) throws AuthentificationException,
+                        EUserException, ELivretException, ESectionException, EQuestionException, EAnswerException {
+                return isAnswerMine(req, answer.getId());
+        }
+
+        /**
+         * <p>
          * Tells if the authenticated user matches the tutor of a livret.
          * </p>
          * 
@@ -431,142 +729,6 @@ public class EntityAccessAuthorization {
         public boolean amITutor(HttpServletRequest req, EAnswer answer) throws AuthentificationException,
                         EUserException, ELivretException, ESectionException, EQuestionException, EAnswerException {
                 return amITutor(req, ar.findById(answer.getId())
-                                .orElseThrow(() -> new EAnswerException(HttpStatus.NO_CONTENT,
-                                                "Answer's id not found."))
-                                .getQuestion());
-        }
-
-        /**
-         * <p>
-         * Tells if the authenticated user matches the owner of a livret.
-         * </p>
-         * 
-         * @see HttpServletRequest
-         * @see ELivret
-         * 
-         * @param req The request where to find the token of the authenticated user.
-         * @param id  The id of the Livret to check from.
-         * @return <i>true</i> if it matches, <i>false</i> otherwise.
-         * 
-         * @throws AuthentificationException
-         * @throws EUserException
-         * @throws ELivretException
-         */
-        public boolean isLivretMine(HttpServletRequest req, Integer id)
-                        throws AuthentificationException, EUserException, ELivretException {
-                EUser ru = service.whoAmI(req);
-
-                ELivret l = lr.findById(id)
-                                .orElseThrow(() -> new ELivretException(HttpStatus.NO_CONTENT,
-                                                "Livret's id not found."));
-
-                EUser lu = ur.findById(l.getUserFromRole(ELivret.UserRole.RESPONSABLE).getId())
-                                .orElseThrow(() -> new EUserException(HttpStatus.NO_CONTENT,
-                                                "Livret's owner not found."));
-
-                return ru.getId() == lu.getId();
-        }
-
-        /**
-         * <p>
-         * Tells if the authenticated user matches the owner of a livret.
-         * </p>
-         * 
-         * @see HttpServletRequest
-         * @see ELivret
-         * @see #isLivretMine(HttpServletRequest, Integer)
-         * 
-         * @param req    The request where to find the token of the authenticated user.
-         * @param livret The livret to check from.
-         * @return <i>true</i> if it matches, <i>false</i> otherwise.
-         * 
-         * @throws AuthentificationException
-         * @throws EUserException
-         * @throws ELivretException
-         */
-        public boolean isLivretMine(HttpServletRequest req, ELivret livret)
-                        throws AuthentificationException, EUserException, ELivretException {
-                return isLivretMine(req, livret.getId());
-        }
-
-        /**
-         * <p>
-         * Tells if the authenticated user matches the owner of a livret.
-         * </p>
-         * 
-         * @see HttpServletRequest
-         * @see ELivret
-         * @see #isLivretMine(HttpServletRequest, Integer)
-         * 
-         * @param req     The request where to find the token of the authenticated user.
-         * @param section The section to check from.
-         * @return <i>true</i> if it matches, <i>false</i> otherwise.
-         * 
-         * @throws AuthentificationException
-         * @throws EUserException
-         * @throws ELivretException
-         * @throws ESectionException
-         */
-        public boolean isLivretMine(HttpServletRequest req, ESection section)
-                        throws AuthentificationException, EUserException, ELivretException, ESectionException {
-                return isLivretMine(req, sr.findById(section.getId())
-                                .orElseThrow(() -> new ESectionException(HttpStatus.NO_CONTENT,
-                                                "Section's id not found."))
-                                .getLivret());
-        }
-
-        /**
-         * <p>
-         * Tells if the authenticated user matches the owner of a livret.
-         * </p>
-         * 
-         * @see HttpServletRequest
-         * @see ELivret
-         * @see #isLivretMine(HttpServletRequest, Integer)
-         * 
-         * @param req      The request where to find the token of the authenticated
-         *                 user.
-         * @param question The question to check from.
-         * @return <i>true</i> if it matches, <i>false</i> otherwise.
-         * 
-         * @throws AuthentificationException
-         * @throws EUserException
-         * @throws ELivretException
-         * @throws ESectionException
-         * @throws EQuestionException
-         */
-        public boolean isLivretMine(HttpServletRequest req, EQuestion question)
-                        throws AuthentificationException, EUserException, ELivretException, ESectionException,
-                        EQuestionException {
-                return isLivretMine(req, qr.findById(question.getId())
-                                .orElseThrow(() -> new EQuestionException(HttpStatus.NO_CONTENT,
-                                                "Question's id not found."))
-                                .getSection());
-        }
-
-        /**
-         * <p>
-         * Tells if the authenticated user matches the owner of a livret.
-         * </p>
-         * 
-         * @see HttpServletRequest
-         * @see ELivret
-         * @see #isLivretMine(HttpServletRequest, Integer)
-         * 
-         * @param req    The request where to find the token of the authenticated user.
-         * @param answer The answer to check from.
-         * @return <i>true</i> if it matches, <i>false</i> otherwise.
-         * 
-         * @throws AuthentificationException
-         * @throws EUserException
-         * @throws ELivretException
-         * @throws ESectionException
-         * @throws EQuestionException
-         * @throws EAnswerException
-         */
-        public boolean isLivretMine(HttpServletRequest req, EAnswer answer) throws AuthentificationException,
-                        EUserException, ELivretException, ESectionException, EQuestionException, EAnswerException {
-                return isLivretMine(req, ar.findById(answer.getId())
                                 .orElseThrow(() -> new EAnswerException(HttpStatus.NO_CONTENT,
                                                 "Answer's id not found."))
                                 .getQuestion());
