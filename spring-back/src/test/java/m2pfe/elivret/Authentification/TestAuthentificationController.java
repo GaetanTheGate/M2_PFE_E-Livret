14e95606f8e7fc71b7e777c1647e86e5b63be3a3
GaetanTheGate <gaetan.tr.pu@gmail.com>
2024-03-22 14:16:36 +0100

    Back - Fixed Authentication Test

--- spring-back/src/test/java/m2pfe/elivret/Authentification/TestAuthentificationController.java
+++ spring-back/src/test/java/m2pfe/elivret/Authentification/TestAuthentificationController.java
@@ -4,102 +4,171 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-import org.junit.jupiter.api.Test;
+import javax.annotation.PostConstruct;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 import m2pfe.elivret.Starter;
+import m2pfe.elivret.EUser.EUser;
 import m2pfe.elivret.EUser.EUserDTO;
+import m2pfe.elivret.EUser.EUserRepository;
+import m2pfe.elivret.UtilityForTest.RestErrorHandlerNoThrows;
 
 @SpringBootTest(classes = Starter.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class TestAuthentificationController {
-    
-    private RestTemplate rest = new RestTemplate();
+    @Autowired
+    public EUserRepository u_repo;
 
+    @Autowired
+    public PasswordEncoder encoder;
+
+    private RestTemplate rest = new RestTemplate();
     private static String BASE_URL = "http://localhost:8081/authentification/";
 
-    private String validLogin(){
+    private String email;
+    private String password;
+
+    @PostConstruct
+    public void init() {
+        rest = new RestTemplate();
+        rest.setErrorHandler(new RestErrorHandlerNoThrows());
+
+        initUser();
+    }
+
+    private void initUser() {
+        email = "email" + this.hashCode();
+        password = "password" + this.hashCode();
+
+        u_repo.save(EUser.builder().email(email).password(encoder.encode(password)).firstName("TEST").lastName("TEST")
+                .build());
+    }
+
+    private String validLogin() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "");
+
         EUserDTO.In.AuthentificationInformation auth = new EUserDTO.In.AuthentificationInformation();
+        auth.setEmail(email);
+        auth.setPassword(password);
 
-        auth.setEmail("email");
-        auth.setPassword("password");
+        ResponseEntity<String> r_login = rest.exchange(BASE_URL + "/login", HttpMethod.POST,
+                new HttpEntity<>(auth, headers), String.class);
 
-        return rest.postForEntity(BASE_URL+"/login", auth, String.class).getBody();
+        return r_login.getBody();
     }
 
     @Test
-    public void testLogin(){
+    public void testLogin() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "");
+
         EUserDTO.In.AuthentificationInformation auth = new EUserDTO.In.AuthentificationInformation();
+        auth.setEmail(email);
+        auth.setPassword(password);
 
-        auth.setEmail("email");
-        auth.setPassword("password");
+        ResponseEntity<String> r_login = rest.exchange(BASE_URL + "/login", HttpMethod.POST,
+                new HttpEntity<>(auth, headers), String.class);
 
-        ResponseEntity<String> response = rest.postForEntity(BASE_URL+"/login", auth, String.class);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, r_login.getStatusCode());
+        assertNotNull(r_login.getBody());
     }
 
     @Test
-    public void testLoginInvalidEmail(){
-        EUserDTO.In.AuthentificationInformation auth = new EUserDTO.In.AuthentificationInformation();
+    public void testLoginInvalidEmail() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "");
 
+        EUserDTO.In.AuthentificationInformation auth = new EUserDTO.In.AuthentificationInformation();
         auth.setEmail("invalid");
-        auth.setPassword("password");
+        auth.setPassword(password);
 
-        ResponseEntity<String> response = rest.postForEntity(BASE_URL+"/login", auth, String.class);
+        ResponseEntity<String> r_login = rest.exchange(BASE_URL + "/login", HttpMethod.POST,
+                new HttpEntity<>(auth, headers), String.class);
 
-        assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
-        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.FORBIDDEN, r_login.getStatusCode());
     }
 
     @Test
-    public void testLoginInvalidPassword(){
+    public void testLoginInvalidPassword() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "");
+
         EUserDTO.In.AuthentificationInformation auth = new EUserDTO.In.AuthentificationInformation();
+        auth.setEmail(email);
+        auth.setPassword("invalid");
 
-        auth.setEmail("email");
-        auth.setPassword("wrong");
+        ResponseEntity<String> r_login = rest.exchange(BASE_URL + "/login", HttpMethod.POST,
+                new HttpEntity<>(auth, headers), String.class);
 
-        ResponseEntity<String> response = rest.postForEntity(BASE_URL+"/login", auth, String.class);
-
-        assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
-        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.FORBIDDEN, r_login.getStatusCode());
     }
 
     @Test
-    public void testLogout(){
+    public void testWhoAmI() {
         String token = validLogin();
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + token);
 
-        HttpEntity<HttpHeaders> entity = new HttpEntity<>(headers);
-        
+        ResponseEntity<EUserDTO.Out.UserInformation> r_whoami = rest.exchange(BASE_URL + "/whoami", HttpMethod.GET,
+                new HttpEntity<>(headers), EUserDTO.Out.UserInformation.class);
 
-        ResponseEntity<String> response = rest.postForEntity(BASE_URL+"/logout", entity, String.class);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertNull(response.getBody());
+        assertEquals(HttpStatus.OK, r_whoami.getStatusCode());
+        assertNotNull(r_whoami.getBody());
     }
 
     @Test
-    public void testLogoutInvalidToken(){
+    public void testWhoAmIInvalidToken() {
+        String token = "token";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+
+        ResponseEntity<EUserDTO.Out.UserInformation> r_whoami = rest.exchange(BASE_URL + "/whoami", HttpMethod.GET,
+                new HttpEntity<>(headers), EUserDTO.Out.UserInformation.class);
+
+        assertEquals(HttpStatus.NO_CONTENT, r_whoami.getStatusCode());
+    }
+
+    @Test
+    public void testLogout() {
+        String token = validLogin();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+
+        ResponseEntity<String> r_logout = rest.exchange(BASE_URL + "/logout", HttpMethod.POST,
+                new HttpEntity<>(headers), String.class);
+
+        ResponseEntity<EUserDTO.Out.UserInformation> r_whoami = rest.exchange(BASE_URL + "/whoami", HttpMethod.GET,
+                new HttpEntity<>(headers), EUserDTO.Out.UserInformation.class);
+
+        assertEquals(HttpStatus.OK, r_logout.getStatusCode());
+        assertEquals(HttpStatus.NO_CONTENT, r_whoami.getStatusCode());
+    }
+
+    @Test
+    public void testLogoutInvalidToken() {
         String token = "invalid";
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + token);
 
-        HttpEntity<HttpHeaders> entity = new HttpEntity<>(headers);
-        
-        
-        ResponseEntity<String> response = rest.postForEntity(BASE_URL+"/logout", entity, String.class);
+        ResponseEntity<String> r_logout = rest.exchange(BASE_URL + "/logout", HttpMethod.POST,
+                new HttpEntity<>(headers), String.class);
 
-        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
-        assertNotNull(response.getBody());
+        // TODO : WhoAmi
+
+        assertEquals(HttpStatus.NO_CONTENT, r_logout.getStatusCode());
     }
 }
